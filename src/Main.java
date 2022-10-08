c1d1333735b14c68709f606a2504a181403a01dc
yasar.bal <ysr.bal@gmail.com>
2022-10-08 19:02:22 +0300

    Entry has been made for testing.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Employee employee = new Employee("Yasar BAL", 9000, 50, 2019);
 
+        String print = employee.toString();
+        System.out.println(print);
+        employee.tax();
+        employee.bonus();
+        employee.raiseSalary();
     }
 }
